3e8e39e9701ba688fb07367fb5d0a90552189a5e
Skilled <skilled5041@gmail.com>
2022-12-29 13:44:02 -0500

    Fixed card_back.png and resized card images

--- test2.py
+++ test2.py
@@ -1,11 +1,11 @@
-import pyautogui
-import time
+# Resize all files in cards to 150 width
+from PIL import Image
 
-pyautogui.moveTo(650, 825, duration=0.25)
-time.sleep(0.5)
+suits = ["diamonds", "clubs", "hearts", "spades"]
+values = ["ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king"]
 
-for i in range(10):
-    # pyautogui.drag(620, 0, 1.5, button='left')
-    # pyautogui.move(-620, -18, 0)
-    pyautogui.drag(0, -600, 1.5, button='left')
-    pyautogui.move(18, 600, 0)
+for i in range(13):
+    for j in range(4):
+        im = Image.open(f"./cards/{values[i]}_of_{suits[j]}.png")
+        im.thumbnail((150, 225))
+        im.save(f"./cards/{values[i]}_of_{suits[j]}.png")
