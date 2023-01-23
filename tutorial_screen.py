3d1271c005acf45357d243870c2061c570b8116c
4seasonz <49500760+4seasonz@users.noreply.github.com>
2023-01-22 21:30:28 -0500

    tutorial screen uploaded

---
+++ tutorial_screen.py
@@ -0,0 +1,20 @@
+from graphics import *
+from game import Game
+
+
+class TutorialScreen:
+    Game.undraw_all()
+    instructions_text = Text(Point(150,150), "The goal of the game is to have a hand value of 21 or as close to 21 as possible without going over (busting).\nThe value of the cards is as follows: 2-10 are face value, aces can be 1 or 11, and face cards (kings, queens, and jacks) are worth 10.\nAfter receiving your initial two cards, you have the option to "hit" and receive additional cards to try and improve your hand, one at a time.\nOnce you are satisfied with your hand, you can "stand" and keep your current hand\nThe dealer will then reveal their second card and play according to a set of rules that dictate when they must hit and when they must stand.\nIf the dealer busts (goes over 21), all remaining players win.\nIf the dealer does not bust, then the player's hands are compared to the dealer's hand and whoever has the hand closest to 21 without going over wins."))
+    
+    instructions_text.setSize(10)
+    instructions_text.draw(Game.window)
+
+
+    @staticmethod
+    def draw_screen(event=None):
+        Game.undraw_all()
+
+   
+
+        
+        
