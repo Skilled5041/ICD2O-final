3eb9cac6d3b731183ea4e0f7f024b6fe9bbef620
skilled5041 <skilled5041@gmail.com>
2023-01-23 10:46:51 -0500

    Fixed open tab

--- screens/tutorial_screen.py
+++ screens/tutorial_screen.py
@@ -9,7 +9,6 @@ class TutorialScreen:
     instructions_blackjack = Button(Point(WIDTH / 4 - 50, HEIGHT / 2 - 25), Point(WIDTH / 4 + 50, HEIGHT / 2 + 25), "Start")
     instructions_blackjack.body.setFill("green")
 
-
     back_btn = Button(Point(0, 600), Point(200, 700), "Back")
     back_btn.body.setFill("white")
 
@@ -19,13 +18,16 @@ class TutorialScreen:
         fn()
 
     @staticmethod
+    def open_link(link):
+        webbrowser.open_new_tab(link)
+
+    @staticmethod
     def draw_screen(event=None):
         Game.undraw_all()
 
-        TutorialScreen.instructions_blackjack(Game.window)
+        TutorialScreen.instructions_blackjack.draw(Game.window)
         TutorialScreen.back_btn.draw(Game.window)
-
         
-        TutorialScreen.instructions_blackjack.bind_click(Game.window, webbrowser.open_new_tab("https://bicyclecards.com/how-to-play/blackjack/"))
+        TutorialScreen.instructions_blackjack.bind_click(Game.window, lambda _: TutorialScreen.open_link("https://bicyclecards.com/how-to-play/blackjack/"))
         from screens.start_screen import StartScreen
         TutorialScreen.back_btn.bind_click(Game.window, lambda _: TutorialScreen.play_pop(StartScreen.start))
