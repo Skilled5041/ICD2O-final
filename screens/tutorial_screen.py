b33821a03cb887d22aab45909c3ba8a46e82cde9
4seasonz <49500760+4seasonz@users.noreply.github.com>
2023-01-23 10:54:00 -0500

    Tutorial Screen Finished?

--- screens/tutorial_screen.py
+++ screens/tutorial_screen.py
@@ -3,11 +3,15 @@ from graphics_extras import Button
 from game import Game
 import webbrowser
 
-WIDTH = 1200
-HEIGHT = 800
+
 class TutorialScreen:
-    instructions_blackjack = Button(Point(WIDTH / 4 - 50, HEIGHT / 2 - 25), Point(WIDTH / 4 + 50, HEIGHT / 2 + 25), "Start")
+    instructions_blackjack = Button(Point(400,200),Point(800,400), "Click for Blackjack Game Rules")
     instructions_blackjack.body.setFill("green")
+    instructions_blackjack.label.setSize(20)
+    instructions_slapjack = Button(Point(400,500),Point(800,700), "Click for Slapjack Game Rules")
+    instructions_slapjack.body.setFill("blue")
+    instructions_slapjack.label.setSize(20)
+    instructions_slapjack.label.setTextColor("white")
 
     back_btn = Button(Point(0, 600), Point(200, 700), "Back")
     back_btn.body.setFill("white")
@@ -27,7 +31,10 @@ class TutorialScreen:
 
         TutorialScreen.instructions_blackjack.draw(Game.window)
         TutorialScreen.back_btn.draw(Game.window)
+        TutorialScreen.instructions_slapjack.draw(Game.window)
         
         TutorialScreen.instructions_blackjack.bind_click(Game.window, lambda _: TutorialScreen.open_link("https://bicyclecards.com/how-to-play/blackjack/"))
+        TutorialScreen.instructions_slapjack.bind_click(Game.window, lambda _: TutorialScreen.open_link("https://bicyclecards.com/how-to-play/slapjack/"))
+        
         from screens.start_screen import StartScreen
         TutorialScreen.back_btn.bind_click(Game.window, lambda _: TutorialScreen.play_pop(StartScreen.start))
